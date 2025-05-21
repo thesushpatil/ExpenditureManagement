ef69f4044c91c547eac47b940f836bdf5b8b5930
thesushpatil <sushantpatil6217@gmail.com>
2025-05-21 11:44:16 +0530

    changes made by sushant

--- Expenses_App/models.py
+++ Expenses_App/models.py
@@ -2,11 +2,11 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class UserData(models.Model):
-    user=models.ForeignKey(User,on_delete=models.CASCADE,null=True,blank=False)
-    name=models.CharField(max_length=100,null=False )
-    email=models.EmailField(max_length=100,null=False)
-    password=models.CharField(max_length=100,null=False)
+# class UserData(models.Model):
+#     user=models.ForeignKey(User,on_delete=models.CASCADE,null=True,blank=False)
+#     name=models.CharField(max_length=100,null=False )
+#     email=models.EmailField(max_length=100,null=False)
+#     password=models.CharField(max_length=100,null=False)
 
 
 
