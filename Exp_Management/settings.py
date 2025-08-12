daa0fb87f52d5c3882464deb20f22ba72dafbd06
thesushpatil <sushantpatil6217@gmail.com>
2025-08-12 19:00:44 +0530

    changes made in hosts

--- Exp_Management/settings.py
+++ Exp_Management/settings.py
@@ -27,8 +27,7 @@ SECRET_KEY = 'django-insecure-dx@tz)$q1r=27w!@p)_q=l=)twod%k7%rg0c)$c$w^760md5ws
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app', 'localhost', '127.0.0.1']
-
+ALLOWED_HOSTS = ['expmanagement.vercel.app', 'localhost', '127.0.0.1']
 
 # Application definition
 
